42cc3f8377acd5c510a921e7a315b067363e20d7
Shaun Leung <sl188@queensu.ca>
2019-11-21 13:58:35 -0500

    finished pokemon name generator

--- Name_Gen.py
+++ Name_Gen.py
@@ -1,35 +1,59 @@
 import pandas as pd
 import random
 
-f = open("Raw_Data.txt",'r')
-
-
-def build_data_base(data,n):
+def build_markov_chain(data, n):
     chain = {
-        '_start':{},
+        '_initial':{},
         '_names': set(data)
     }
-
-    for name in data:
-        word_wrapped = str(name) + '.'
-        for i in range(0,word_wrapped - n):
-            tuple = word_wrapped[i:i+n]
-            next = word_wrapped[i+1:i+1]
-        
+    for word in data:
+        word_wrapped = str(word) + '.'
+        for i in range(0, len(word_wrapped) - n):
+            tuple = word_wrapped[i:i + n]
+            next = word_wrapped[i + 1:i + n + 1]
+            
             if tuple not in chain:
                 entry = chain[tuple] = {}
             else:
                 entry = chain[tuple]
-
-            if i ==0:
-                if tuple not in chain[tuple]:
-                    chain['_start'][tuple] = 1
-                else:
-                    chain['_start'][tuple] += 1
             
+            if i == 0:
+                if tuple not in chain['_initial']:
+                    chain['_initial'][tuple] = 1
+                else:
+                    chain['_initial'][tuple] += 1
+                    
             if next not in entry:
                 entry[next] = 1
             else:
                 entry[next] += 1
+    return chain 
 
-    return chain
+def select_random_item(items):
+    rnd = random.random() * sum(items.values())
+    for item in items:
+        rnd -= items[item]
+        if rnd < 0:
+            return item 
+
+def generate(chain):
+    tuple = select_random_item(chain['_initial'])
+    result = [tuple]
+    
+    while True:
+        tuple = select_random_item(chain[tuple])
+        last_character = tuple[-1]
+        if last_character == '.':
+            break
+        result.append(last_character)
+    
+    generated = ''.join(result)
+    if generated not in chain['_names']:
+        return generated
+    else:
+        return generate(chain)
+
+
+data_frame = pd.read_csv("Raw_Data.csv")
+
+print(generate(build_markov_chain(data_frame['name'].tolist(),3)))
