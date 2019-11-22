f9faa72ec5712f3d9b5e5912c666fc490701c148
Shaun Leung <sl188@queensu.ca>
2019-11-22 10:40:43 -0500

    changed code to filters and maps for pharma

--- Name_Gen.py
+++ Name_Gen.py
@@ -6,15 +6,8 @@ pkmn = pd.read_csv("pkmn.csv")
 pharma = pd.read_csv("pharma.csv")
 philo = pd.read_csv("philo.csv")
 
-db = []
-for entry in pharma['brand_name']:
-    if entry.isalpha():
-        db.append(entry)
-db = list(dict.fromkeys(db))
-
-newdb = []
-for item in db:
-    newdb.append( str(item[0]).upper() + str(item[1:]).lower() )
+db = list(filter(lambda x: x.isalpha() ,pharma['brand_name']))
+db = list(map(lambda item: str(item[0]).upper() + str(item[1:]).lower(), db ))
 
 for i in range(0,15):
-    print(mc.generate(mc.build_markov_chain(newdb,3)))
+    print(mc.generate(mc.build_markov_chain(db,3)))
