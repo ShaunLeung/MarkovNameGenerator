39a6906204fcc0b524504adb2c398a1f1de71bec
Shaun Leung <sl188@queensu.ca>
2019-11-22 15:49:10 -0500

    unified database handling

--- Name_Gen.py
+++ Name_Gen.py
@@ -6,8 +6,9 @@ pkmn = pd.read_csv("pkmn.csv")
 pharma = pd.read_csv("pharma.csv")
 philo = pd.read_csv("philo.csv")
 
-db = list(filter(lambda x: x.isalpha() ,pharma['brand_name']))
-db = list(map(lambda item: str(item[0]).upper() + str(item[1:]).lower(), db ))
+
+db = list(pharma['name']) + list(pkmn['name']) + list(philo['name'])
 
 for i in range(0,15):
     print(mc.generate(mc.build_markov_chain(db,3)))
+
